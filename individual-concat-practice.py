c2ddefc00e0f8cfdc12cb9583a2244c921389cd1
SoggyCereal <PhillipsJo860@northwested.org>
2024-09-11 10:49:55 -0400

    added content

--- individual-concat-practice.py
+++ individual-concat-practice.py
@@ -5,6 +5,11 @@
 # Part 1
 # Use concatenation to build and display a string that displays which city and state you live in
 
+# CODE STARTS HERE----->
+# city = 'Mancelona'
+# state = 'MI'
+
+# print('I live in ' + city + ' ' + state)
 
 
 
@@ -14,7 +19,10 @@
 # Create another variable that stores the welcome message
 # Use concatenation to create the customized message
 
-
+# CODE STARTS HERE----->
+# player_name = input('Please enter your username: ')
+# message = 'Welcome to (INSERT GAME NAME HERE) ' + player_name + ', PRESS START!'
+# print(message)
 
 
 # Part 3
@@ -22,10 +30,16 @@
 # Use the built-in Python str( ) function to convert your age to a string (when doing concatenation)
 # Use concatenation to display a sentence that tells us your first name and age
 
-
-
-
+# CODE STARTS HERE----->
+# age = 6000
+# print('Hello my name in comet destroyer im ' + str(age) + ' years old and hate all existance.')
 
 
 # Part 4
 # Use an f-string to build and display a string that contains your first name, last name, and your height in inches
+
+# CODE STARTS HERE----->
+first = 'Joshua'
+last = 'Phillips'
+height = '(IDK maybe 71 inches.)'
+print(f'My name is {first} {last} and im {height} tall.')
--- paired-concat-practice.py
+++ paired-concat-practice.py
@@ -10,23 +10,34 @@
 # Part 1
 # Create variables to store your first, middle, and last name
 # Have Python print a message that contains your concatenated full name, i.e., your combined first, middle and last names
-first = ('Joshua')
-middle = ('James')
-last = ('Phillips')
-full_name = first + '' + middle + '' + last
-print(f'Hemmo my name is: {full_name}')
+
+# first_name = 'Joshua'
+# middle_name = 'James'
+# last_name = 'Phillips'
+# print(first_name+' '+middle_name+' '+last_name)
 
 # Part 2
 # Assume you're building a Space Invaders game
 # Use concatenation to create and display a custom welcome message that includes the player's first name
 
+# game = 'Space Invaders'
+# player_name = input('enter name here: ')
+# print(f'Hello {player_name} and welcome to {game}!')
 
 
 # Part 3
 # Use concatenation to build and display a string that includes your first name, last name, and the year you were born
 
+# first = 'Joshua'
+# last = 'Phillips'
+# year = '2005'
+# print(first + ' ' + last + ' ' + year)
 
 
 # Part 4
 # Use concatenation to create and display a sentence that says which country in the world
 # you would visit if money were no object
+
+place = 'Japan'
+
+print('If I were to visit somplace as if money wasnt an issue I would go to ' + place + ' and would most likely not want to leave. (If it werent for the language gap.)')
